194c7a82a3320a1a5d217c56366d547950094adc
1iuyuhe <liuyuhe1024@foxmail.com>
2023-02-24 15:17:00 +0800

    😘: 2023-02-24 15:17:00

--- submit.sh
+++ submit.sh
@@ -1,3 +1,19 @@
+#!/bin/bash
+function rand() {
+        min=$1
+        max=$(($2-$min+1))
+        num=$(date +%s%N)
+        echo $(($num%$max+$min))
+}
+
+function get_emoji() {
+	emoji_list=("😘" "🎉" "🎂" "🥳" "🎈" "🎊" "🎁" "🧁" "🥂" "🍰" "🪅" "🎆" "🤩" "🌼" "🌷" "🍨" "💗")
+	i=$(rand 0 len-1)
+	echo ${emoji_list[$i]}
+}
+emoji=$(get_emoji)
+echo "--------"
 git add .
-git commit -m "$(date)"
+git commit -m "$emoji: $(date '+%Y-%m-%d %T')"
 git push
+echo "--------"
